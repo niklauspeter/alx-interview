f9090f97e7e1bfd9ccc7a31ad4caa59764be2224
klaus <oriokiklaus@gmail.com>
2023-11-09 18:32:28 +0300

    modify endpoint

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,7 +4,7 @@ const request = util.promisify(require('request'));
 const filmID = process.argv[2];
 
 async function starwarsCharacters (filmId) {
-  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
+  const endpoint = 'https://swapi-api.alx-tools.com/api/films/' + filmId;
   let response = await (await request(endpoint)).body;
   response = JSON.parse(response);
   const characters = response.characters;
