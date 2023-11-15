fe01728ae4ea92aabcc590f737a6000d3b1ab7a9
klaus <oriokiklaus@gmail.com>
2023-11-15 22:22:37 +0300

    script rotates 2 D matrix on spot

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+two D matrix provided as input
+rotated 90 degrees clockwise
+"""
+
+def rotate_2d_matrix(matrix):
+    """
+    Given an n x n 2D matrix, rotate it 90 degrees clockwise.
+    """
+    n = len(matrix)
+    for layer in range(n // 2):
+        first, last, offset = layer, n - 1 - layer, 0
+        for i in range(first, last):
+            top = matrix[first][i]
+            matrix[first][i] = matrix[last - offset][first]
+            matrix[last - offset][first] = matrix[last][last - offset]
+            matrix[last][last - offset] = matrix[i][last]
+            matrix[i][last] = top
+            offset += 1
+
+# def arrRotation(A):
+#     # A = [[1, 2, 3],
+#     #      [4, 5, 6],
+#     #      [7, 8, 9]]
+
+#     Output = []
+#     for i in range(len(A)):
+#         row = []
+#         for j in range(len(A[0])):
+#             row.insert(i, A[i])
+#         Output.append(row)
+#     return Output
---
+++ 0x07-rotate_2d_matrix/main_0.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+test for 90 degree 2-d array matrix rotation
+"""
+
+rotate_2d_matrix = __import__('0-rotate_2d_matrix').rotate_2d_matrix
+
+if __name__ == "__main__":
+    matrix = [[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]]
+
+    rotate_2d_matrix(matrix)
+    print(matrix)
