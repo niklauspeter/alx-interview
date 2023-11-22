b8dda22101070d7099308ac0cf6284296a2d2df7
klaus <oriokiklaus@gmail.com>
2023-11-22 23:40:48 +0300

    make more eficient by optmizing initialization

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -14,7 +14,7 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    dp = [0] + [float("inf")] * (total)
+    dp = [0] + [float("inf")] * total
     for coin in coins:
         for i in range(coin, total + 1):
             dp[i] = min(dp[i], dp[i - coin] + 1)
